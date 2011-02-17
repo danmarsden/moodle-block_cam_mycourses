8ea1165c6ce51fa4cadac9e8275f39ff2e6c9057
Dan Marsden <dan@catalyst.net.nz>
2011-02-18 10:35:11 +1300

    improve locking on admin form based on cascade checkbox - needs further css work

--- displaycategories.php
+++ displaycategories.php
@@ -24,6 +24,7 @@
  */
 require_once(dirname(dirname(__FILE__)) . '/../config.php');
 require_once($CFG->dirroot.'/course/lib.php');
+require_once('displaycategories_form.php');
 
 require_login();
 $context = get_context_instance(CONTEXT_SYSTEM);
@@ -44,9 +45,9 @@ $PAGE->navbar->add(get_string('pluginname', 'block_cam_mycourses'), $settingsurl
 $PAGE->navbar->add($strcatsettings, $categoriesurl);
 echo $OUTPUT->header();
 
-make_categories_list(&$list, &$parents);
-
+$mform = new mycourse_categories_form('');
 if (data_submitted() && confirm_sesskey()) {
+    make_categories_list(&$list, &$parents);
     foreach ($list as $cid => $notused) {
         set_config('display_'.$cid, optional_param('display'.$cid, 0, PARAM_INT),'block_cam_mycourses');
         set_config('cascade_'.$cid, optional_param('cascade'.$cid, 0, PARAM_INT),'block_cam_mycourses');
@@ -55,88 +56,6 @@ if (data_submitted() && confirm_sesskey()) {
     echo $OUTPUT->notification(get_string('settingssaved','block_cam_mycourses'), 'notifysuccess');
 }
 
-echo '<form method="post" action="displaycategories.php">';
-echo '<table class="generalbox editcourse boxaligncenter"><tr class="header">';
-echo '<th class="header" scope="col">'.get_string('categories').'</th>';
-echo '<th class="header" scope="col">'.get_string('display', 'block_cam_mycourses').'</th>';
-echo '<th class="header" scope="col">'.get_string('cascade', 'block_cam_mycourses').'</th>';
-echo '<th class="header" scope="col">'.get_string('enrolled', 'block_cam_mycourses').'</th>';
-echo '</tr>';
-
-print_category_edit(NULL, $list, $parents);
-echo '<tr><td colspan="4" align="center">';
-echo '<input type="hidden" name="sesskey" value="'.sesskey().'"/>';
-echo '<input type="submit" value="' . get_string("savechanges") . '"\" /></td></tr>';
-echo '</table>';
-echo '</form>';
-
+$mform->display();
 
 echo $OUTPUT->footer();
-
-
-function print_category_edit($category, $displaylist, $parentslist, $depth=-1, $up=false, $down=false) {
-/// Recursive function to print all the categories ready for editing
-
-    global $CFG, $USER, $OUTPUT;
-
-    static $str = NULL;
-    static $config = NULL;
-
-    if (is_null($config)) {
-        $config = get_config('block_cam_mycourses');
-    }
-
-    if (is_null($str)) {
-        $str = new stdClass;
-        $str->spacer = $OUTPUT->spacer().' ';
-        $str->full = get_string('full', 'block_cam_mycourses');
-        $str->listing = get_string('listing', 'block_cam_mycourses');
-        $str->no = get_string('no');
-    }
-    $options = array(1=>$str->full, 2=>$str->listing);
-    if (!empty($category)) {
-
-        if (!isset($category->context)) {
-            $category->context = get_context_instance(CONTEXT_COURSECAT, $category->id);
-        }
-
-        echo '<tr><td align="left" class="name">';
-        for ($i=0; $i<$depth;$i++) {
-            echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-        }
-        echo format_string($category->name);
-        echo '</td>';
-        $display = "display_".$category->id;
-        $display = isset($config->$display) ? $config->$display : 0;
-        $enrolled = "enrol_".$category->id;
-        $enrolled = isset($config->$enrolled) ? $config->$enrolled : false;
-        $cascade = "cascade_".$category->id;
-        $cascade = isset($config->$cascade) ? $config->$cascade : false;
-
-        echo html_writer::tag('td', html_writer::select($options,'display'.$category->id, $display,array(0=>$str->no)));
-        echo html_writer::tag('td', html_writer::checkbox('cascade'.$category->id, 1, $cascade));
-        echo html_writer::tag('td', html_writer::checkbox('enrolled'.$category->id, 1, $enrolled));
-
-        echo '</tr>';
-    } else {
-        $category->id = '0';
-    }
-
-    if ($categories = get_categories($category->id)) {   // Print all the children recursively
-        $countcats = count($categories);
-        $count = 0;
-        $first = true;
-        $last = false;
-        foreach ($categories as $cat) {
-            $count++;
-            if ($count == $countcats) {
-                $last = true;
-            }
-            $up = $first ? false : true;
-            $down = $last ? false : true;
-            $first = false;
-
-            print_category_edit($cat, $displaylist, $parentslist, $depth+1, $up, $down);
-        }
-    }
-}
---
+++ displaycategories_form.php
@@ -0,0 +1,124 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * The form for configuring which services are subscribed and published on a host
+ *
+ * @package    blocks
+ * @subpackage cam_mycourses
+ * @author     Dan Marsden
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+if (!defined('MOODLE_INTERNAL')) {
+    die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
+}
+
+require_once($CFG->libdir . '/formslib.php');
+
+class mycourse_categories_form extends moodleform {
+
+    function definition() {
+        $mform =& $this->_form;
+
+        make_categories_list(&$list, &$parents);
+        $this->print_category_edit(NULL, $list, $parents);
+        $this->add_action_buttons();
+    }
+
+    function print_category_edit($category, $displaylist, $parentslist, $depth=-1, $up=false, $down=false, $thisparents=array()) {
+    /// Recursive function to print all the categories ready for editing
+        global $CFG, $USER, $OUTPUT;
+        $mform =& $this->_form;
+
+        static $str = NULL;
+        static $config = NULL;
+
+        if (is_null($config)) {
+            $config = get_config('block_cam_mycourses');
+        }
+
+        if (is_null($str)) {
+            $str = new stdClass;
+            $str->spacer = $OUTPUT->spacer().' ';
+            $str->full = get_string('full', 'block_cam_mycourses');
+            $str->listing = get_string('listing', 'block_cam_mycourses');
+            $str->no = get_string('no');
+            $str->display = get_string('display', 'block_cam_mycourses');
+            $str->cascade = get_string('cascade', 'block_cam_mycourses');
+            $str->enrol = get_string('enrolled', 'block_cam_mycourses');
+        }
+        $options = array(1=>$str->full, 2=>$str->listing);
+        if (!empty($category)) {
+
+            if (!isset($category->context)) {
+                $category->context = get_context_instance(CONTEXT_COURSECAT, $category->id);
+            }
+
+           // echo '<tr><td align="left" class="name">';
+            $categoryprefix = '';
+            for ($i=0; $i<$depth;$i++) {
+                $categoryprefix = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+            }
+            //echo '</td>';
+            $display = "display_".$category->id;
+            $display = isset($config->$display) ? $config->$display : 0;
+            $enrolled = "enrol_".$category->id;
+            $enrolled = isset($config->$enrolled) ? $config->$enrolled : false;
+            $cascade = "cascade_".$category->id;
+            $cascade = isset($config->$cascade) ? $config->$cascade : false;
+            $mform->addElement('header', 'header'.$category->id, $categoryprefix.format_string($category->name));
+            $mform->addElement('select', 'display'.$category->id, $str->display, $options);
+            $mform->setDefault('display'.$category->id, $display);
+            $mform->addElement('checkbox', 'cascade'.$category->id, $str->cascade);
+            $mform->setDefault('cascade'.$category->id, $cascade);
+            $mform->addElement('checkbox', 'enrolled'.$category->id, $str->enrol);
+            $mform->setDefault('enrolled'.$category->id, $enrolled);
+            foreach($thisparents as $parentid) {
+                $mform->disabledIf('display'.$category->id, 'cascade'.$parentid, 'checked');
+                $mform->disabledIf('cascade'.$category->id, 'cascade'.$parentid, 'checked');
+                $mform->disabledIf('enrolled'.$category->id, 'cascade'.$parentid, 'checked');
+            }
+        } else {
+            $category->id = '0';
+        }
+
+        if ($categories = get_categories($category->id)) {   // Print all the children recursively
+            $countcats = count($categories);
+            $count = 0;
+            $first = true;
+            $last = false;
+            if (!empty($category->id)) {
+                $thisparents[] = $category->id;
+            } else {
+                $thisparents = array();
+            }
+            foreach ($categories as $cat) {
+                $count++;
+                if ($count == $countcats) {
+                    $last = true;
+                }
+                $up = $first ? false : true;
+                $down = $last ? false : true;
+                $first = false;
+
+                $this->print_category_edit($cat, $displaylist, $parentslist, $depth+1, $up, $down, $thisparents);
+            }
+        }
+    }
+}
